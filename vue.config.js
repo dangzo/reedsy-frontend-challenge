6beff233bcc0c0325d3a44ebd417c8534e9d31c2
Daniele Gazzelloni <daniele@danielegazzelloni.com>
2019-07-23 15:35:39 +0100

    SCSS folder structure. Styled footer.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ module.exports = {
   devServer: {
     port: 3030
   },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/styles/base.scss";`
+      }
+    },
+  },
   lintOnSave: true,
   publicPath: ''
 };
